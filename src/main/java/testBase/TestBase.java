133caa15a312b12583fad6c7474eb7288f5b378f
Krishnendu <krishnendunandi1990@gmail.com>
2022-03-13 01:03:10 +0530

    begining

--- src/main/java/testBase/TestBase.java
+++ src/main/java/testBase/TestBase.java
@@ -1,5 +1,33 @@
 package testBase;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+
 public class TestBase {
+	
+	static WebDriver driver;
+	static Properties prop; 
+	
+	
+	public TestBase(){
+		
+		try{
+			FileInputStream fis = new FileInputStream("F:\\WorkSpaceForAutomationArchitect\\SharedFrameWork\\src\\test\\resources\\Global_Setting");
+			prop.load(fis);
+		}
+		catch(Exception ex){
+			
+		}
+		
+	}
+	
+	
+	public static void initialization(){
+		
+		
+	}
 
 }
